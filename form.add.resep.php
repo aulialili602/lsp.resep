61cc1b2d66c7699876c032137c972fbb56bfdf0e
aulialili602 <aulialili602@gmail.com>
2025-06-07 22:39:17 +0700

    pull

--- form.add.resep.php
+++ form.add.resep.php
@@ -57,7 +57,7 @@
     <!-- Brand Logo -->
     <a href="index3.html" class="brand-link">
       <img src="" alt="" class="brand-image img-circle elevation-3" style="opacity: .8">
-      <span class="brand-text font-weight-light">Dapur Mamak'e</span>
+      <span class="brand-text font-weight-light">Dapur Mamake</span>
     </a>
 
     <!-- Sidebar -->
